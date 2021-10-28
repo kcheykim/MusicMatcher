0f163bf5dc94bae98c04733a4c979f3f6118f06a
Kurtis <gears2525@aol.com>
2021-10-27 22:14:23 -0700

    Added TasteDive Fetch request
    Had to use a proxy to bypass CORS issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
-console.log('hello world')
+let TasteDiveData;
+let NapsterData;
+let search;
+const apiKey = {
+    TasteDive: '426208-MusicRec-QH74RS8V',
+    Napster: 'MDc1YWUxMWUtYjY0NS00ZGI5LTgxNzEtZjRmMWY0NGQ3Nzgx'
+}
+
+function getSongData(search = 'metallica') {
+    let TasteDiveApi = `https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=${search}&k=${apiKey.TasteDive}`
+    fetch(TasteDiveApi)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    TasteDiveData = data.Similar.Results
+                    console.log(TasteDiveData);
+                });
+            } else {
+                return("Error: " + response.statusText);
+            };
+        })
+        .catch(function (error) {
+            return("Unable to find search result");
+        });
+};
+
+getSongData()
