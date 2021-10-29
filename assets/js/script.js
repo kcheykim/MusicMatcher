51e6f6678c854ca1fc1c2a0e6054862d4c35d378
Ricky Leung <ricxx0320@gmail.com>
2021-10-29 01:43:11 -0700

    building getSong function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,23 +6,28 @@ var searchResults = input.value;
 
 
 
-function getSong() {
-    fetch(apiUrl + searchResults)
+function getSong(songName) {
+    fetch(apiUrl + songName + searchResults)
     .then(function(response) {
-        if (response.status !=200) {
-            console.log('Something went Wrong' + response.status)
-            return;
+        if (response.ok) {
+            response.json().then(function(data) {
+                var keyId = data.dt;
+                localStorage.setItem(keyId, songName);
+                var song1 = $('<button class="song1" data-id="song1"></button>').text(songName);
+                $(".title").append(song1);
+
+                var artist = $('<h2 class="artist"></h2>').text("")
+                $(".is-warning").append(artist);
+
+                var lyrics = $('<p class="lyrics"></p>').text()
+                $(".is-danger").append(lyrics);
+
+            });
+        } else {
+            var errorMessage = $('<p class ="is-info"><p/>').text("Please seach again");
+            $(".is-info").append(errorMessage);
         }
-        else {
-            response.json()
-            .then(function(data) {
-                console.log(data);
-            })
-        }
-    })
+    });
 }
-
-
-
 
 submitBtnEl.on('click', getSong);
