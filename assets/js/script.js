ba0a0a1715c9f1b3099be2ac54b56cf6c5529512
Ricky Leung <ricxx0320@gmail.com>
2021-10-28 22:42:50 -0700

    starting off with variables and click event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
-console.log('hello world')
+var apiUrl = ''
+var apiKey = ''
+var submitBtnEl = $('.searchBtn');
+var input = $('input')
+var searchResults = input.value;
+
+
+
+function getSong() {
+    fetch(apiUrl + searchResults)
+    .then(function(response) {
+        if (response.status !=200) {
+            console.log('Something went Wrong' + response.status)
+            return;
+        }
+        else {
+            response.json()
+            .then(function(data) {
+                console.log(data);
+            })
+        }
+    })
+}
+
+
+
+
+submitBtnEl.on('click', getSong);
