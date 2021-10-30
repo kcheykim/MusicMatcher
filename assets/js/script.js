2c1168227c23b10a5d7c3918c5be5a9092b4c18f
Kurtis <gears2525@aol.com>
2021-10-30 16:08:23 -0700

    Removed index2 & removed code that was merged

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,4 @@
-<<<<<<< HEAD
-var apiUrl = ''
-var apiKey = ''
-var submitBtnEl = $('.searchBtn');
-var input = $('input')
-var searchResults = input.value;
 
-
-
-function getSong(songName) {
-    fetch(apiUrl + songName + searchResults)
-    .then(function(response) {
-        if (response.ok) {
-            response.json().then(function(data) {
-                var keyId = data.dt;
-                localStorage.setItem(keyId, songName);
-                var song1 = $('<button class="song1" data-id="song1"></button>').text(songName);
-                $(".title").append(song1);
-
-                var artist = $('<h2 class="artist"></h2>').text("")
-                $(".is-warning").append(artist);
-
-                var lyrics = $('<p class="lyrics"></p>').text()
-                $(".is-danger").append(lyrics);
-
-            });
-        } else {
-            var errorMessage = $('<p class ="is-info"><p/>').text("Please seach again");
-            $(".is-info").append(errorMessage);
-        }
-    });
-}
-
-submitBtnEl.on('click', getSong);
-=======
 let TasteDiveData;
 let NapsterData;
 let search;
@@ -59,4 +25,3 @@ function getSongData(search = 'metallica') {
 };
 
 getSongData()
->>>>>>> e103c059ca34e043b67891ed6021080663b05edd
