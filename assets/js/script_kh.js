d847178dbe256c0e262d187c0afd18747aa57925
Kurtis <gears2525@aol.com>
2021-10-31 16:49:36 -0700

    Update script_kh.js
    Swapped out tastedive with last.fm

--- assets/js/script_kh.js
+++ assets/js/script_kh.js
@@ -1,46 +1,33 @@
-
-let search;
-// I will create new api keys and add them to apiKey.js
-const apiKey = {
-  TasteDive: '426208-MusicRec-QH74RS8V',
-  Napster: 'MDc1YWUxMWUtYjY0NS00ZGI5LTgxNzEtZjRmMWY0NGQ3Nzgx'
-}
-//hiding Spoitfy client and client secret key in file apiKey.js
-import * as Spotify from "./apiKey.js"
+let search = 'metallica';
+import * as apiKey from "./apiKey.js"
 
 const _getToken = async () => {
-  const result = await fetch('https://accounts.spotify.com/api/token', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Authorization': 'Basic ' + btoa(Spotify.clientID + ':' + Spotify.clientSecret)
-    },
-    body: 'grant_type=client_credentials'
-  })
-  const data = await result.json();
-  console.log(data.access_token);
+    const result = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + btoa(apiKey.SpotifyclientID + ':' + apiKey.SpotifyclientSecret)
+        },
+        body: 'grant_type=client_credentials'
+    })
+    const data = await result.json();
+    return data.access_token
+};
+
+const _getSimilarArtist= async(search) => {
+    const lastFM = `https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=${search}&api_key=${apiKey.lastFmKey}&format=json`
+    const result = await fetch(lastFM)
+    const data = await result.json();
+    return data.similarartists.artist
+}
+const _searchArtist= async (search) => {
+    const result = await fetch('', {
+        method: 'GET',
+        headers:{'Authorization' : `Bearer ${SpotifyToken}`}
+    })
+
 }
 
-function getSongData(search) {
-  //Visit the proxy link and enable for taste dive
-  const proxy = 'https://cors-anywhere.herokuapp.com/'
-  const TasteDiveApi = `${proxy}https://tastedive.com/api/similar?q=${search}&k=${apiKey.TasteDive}`
-  const napsterApi = "https://api.napster.com/v2.2/artists/top";
-  const options = {
-    headers: {
-      apikey: apiKey.Napster
-    }
-  };
-
-  fetch(TasteDiveApi)
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
-
-  fetch(napsterApi, options)
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
-};
-getSongData()
-_getToken()
+const SpotifyToken = _getToken()
+const SimilarArtist = _getSimilarArtist(search)
+console.log(SimilarArtist)
