bac0d6636f4d5cc9bcd525f4dc6f8bb94861c41a
Kurtis <gears2525@aol.com>
2021-11-01 19:34:19 -0700

    Removed Spotify and clean up code

--- assets/js/apiKey.js
+++
@@ -1,3 +0,0 @@
-
-export const clientID = ''
-export const clientSecret = ''
--- assets/js/script_kh.js
+++ assets/js/script_kh.js
@@ -1,33 +1,11 @@
 let search = 'metallica';
-import * as apiKey from "./apiKey.js"
-
-const _getToken = async () => {
-    const result = await fetch('https://accounts.spotify.com/api/token', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Basic ' + btoa(apiKey.SpotifyclientID + ':' + apiKey.SpotifyclientSecret)
-        },
-        body: 'grant_type=client_credentials'
-    })
-    const data = await result.json();
-    return data.access_token
-};
+const lastFmKey = 'f50b0e7f874bf3ca9a40af2dc2697097'
 
 const _getSimilarArtist= async(search) => {
-    const lastFM = `https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=${search}&api_key=${apiKey.lastFmKey}&format=json`
+    const lastFM = `https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=${search}&api_key=${lastFmKey}&format=json`
     const result = await fetch(lastFM)
     const data = await result.json();
     return data.similarartists.artist
 }
-const _searchArtist= async (search) => {
-    const result = await fetch('', {
-        method: 'GET',
-        headers:{'Authorization' : `Bearer ${SpotifyToken}`}
-    })
 
-}
-
-const SpotifyToken = _getToken()
 const SimilarArtist = _getSimilarArtist(search)
-console.log(SimilarArtist)
