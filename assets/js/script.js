470364ce8a14885e69c0fb9393b1000cfff668b1
Kurtis <gears2525@aol.com>
2021-11-02 19:08:14 -0700

    Removed index buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,67 @@
+const lastFmKey = 'f50b0e7f874bf3ca9a40af2dc2697097'
+let oldSearch = [];
+
+const _getSimilarArtist = async (search) => {
+    const lastFM = `https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=${search}&api_key=${lastFmKey}&format=json&limit=5`
+    const result = await fetch(lastFM)
+    const data = await result.json();
+    console.log(data)
+    return data.similarartists.artist
+}
+
+const _getTopTracks = async (artistName) => {
+    const lastFM = `https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=${artistName}&api_key=${lastFmKey}&format=json&limit=5`
+    const result = await fetch(lastFM)
+    const data = await result.json();
+    console.log(data)
+    return data.toptracks.track
+}
+
 //submit button element for adding click event for our search
 let searchEl = document.getElementById('search-artist');
 
 
 function getArtist(event) {
-    debugger;
     event.preventDefault();
     if (searchEl) {
-
         let artist = document.getElementById('artist-input').value;
-        debugger;
         if (!artist) {
             return;
-        }
+        }   
+        oldSearch.push(artist);
+        localStorage.setItem("artist", JSON.stringify(oldSearch));
+        artist.value = "";
 
         _getSimilarArtist(artist);
 
         getLyric(artist); //need to get rid of this once pass the artist from Kurtis function
-    }
-}
+
+    }    
+}    
 
 searchEl.addEventListener('click', getArtist);
-debugger;
 
-
+function loadOldSearch() {
+    try {
+      oldSearch = JSON.parse(localStorage.getItem("artist"));
+      for (let i = 0; i < oldSearch.length; i++) {
+        let searchEl = document.createElement(`button${i}`);
+        searchEl.textContent = `${oldSearch[i]}`;
+        searchEl.classList.add('btn-results');
+        searchEl.classList.add('is-medium mt-1');
+        document.querySelector("#search-results").appendChild(searchEl);
+      };
+      let savedResults = document.querySelectorAll(".btn-results");
+      for (let i = 0; i < savedResults.length; i++) {
+        savedResults[i].addEventListener('click', function () {
+          nameInputEl.value = savedResults[i].innerHTML;
+        });
+      };
+    } catch (error) {
+      console.log(error)
+      oldSearch = [];
+    }
+  }
 
 //-------------------------------------------------------------------------------//
 
@@ -43,40 +82,23 @@ let top5 = document.getElementById("button5");
 
 //-------------------------------------------------------------------------------//
 
-const lastFmKey = 'f50b0e7f874bf3ca9a40af2dc2697097'
-
-const _getSimilarArtist = async(search) => {
-    const lastFM = `https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=${search}&api_key=${lastFmKey}&format=json&limit=5`
-    const result = await fetch(lastFM)
-    const data = await result.json();
-    return data.similarartists.artist
-}
-
-const _getTopTracks = async(artistName) => {
-    const lastFM = `https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=${artistName}&api_key=${lastFmKey}&format=json&limit=5`
-    const result = await fetch(lastFM)
-    const data = await result.json();
-    return data.toptracks.track
-}
 
 //replace metallica and  Daft Punk with artists name 
 const SimilarArtist = _getSimilarArtist('metallica')
 const artistTopTracks = _getTopTracks('Daft Punk')
 
 
-function getLyric(artist) {
+function getLyric(artist , song) {
     let lyricEl = document.getElementById('lyric');
     lyricEl.innerHTML = "";
-    debugger;
-    let song = prompt('Please type in a song: ');
     if (!song) {
         return;
     }
     let lyricApi = 'https://api.lyrics.ovh/v1/';
     fetch(lyricApi + artist + '/' + song)
-        .then(function(response) {
+        .then(function (response) {
             if (response.ok) {
-                response.json().then(function(data) {
+                response.json().then(function (data) {
                     console.log(data.lyrics);
                     var parseLyric = data.lyrics.split('\r\n')[1];
                     parseLyric = replaceStr(parseLyric, '\n\n\n', '|');
